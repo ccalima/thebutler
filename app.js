706764f87903c489f48717aebecbdd44559a823c
Robert Han <rhan@highground.com>
2018-01-03 12:06:44 -0600

    !??!

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ var doc = new GoogleSpreadsheet('1E1o3uWXYlDfo79pHzHkFHqGGPZjZikfCw53ITLHnNTs');
 
 // enrage server information
 var guild, roles, channels, members,
-	officer, staticleader, raider, crafter, social, guest,
-	entrancehall,
-	applicants = {},
-	messagesToDelete,
-	requested;
+officer, staticleader, raider, crafter, social, guest,
+entrancehall,
+applicants = {},
+messagesToDelete,
+requested;
 
 client.on('ready', () => {
 	console.log('I am ready!');
@@ -64,52 +64,52 @@ client.on('message', message => {
 			if (typeof applicants[message.author.username] !== 'string') {
 				applicants[message.author.username] = 'false';
 				message.guild.createChannel(message.author.username.replace(/\s+/g, '-').replace(/[^\x00-\x7F]/g, '').replace(/\W/g, '').toLowerCase().toLowerCase(), 'text')
-					.then(function (channel) {
-						channel.overwritePermissions(message.author, {
-							READ_MESSAGES: true,
-							SEND_MESSAGES: true
-						});
-						channel.overwritePermissions(everyone, {
-							READ_MESSAGES: false,
-							SEND_MESSAGES: false
-						});
-						channel.overwritePermissions(officer, {
-							READ_MESSAGES: true,
-							SEND_MESSAGES: true
-						});
-						message.author.send("Here are the questions you are expected to answer. Please answer them thoroughly and to the best of your knowledge, as this is Enrage's first impression of you. \nOnce you have completed all the questions, **please copy/paste the questions and answers into the created text channel named after you in the Enrage Discord.**");
-						message.author.send("You must complete this process within 24 hours. Failure to do so will result in the immediate rejection of your application. \nAlso note that your application will be scrutinized by both the officers and the members.");
-						message.author.send("With that, here are the questions: \n1). What is your character's name? \n2). What made you choose Enrage? What are your expectations of us?");
-						if (message.content === '-Raider') {
-							message.author.send("3). What class(es) do you raid on? \n4). Please link your Lodestone with your Achievements set to be viewed publically. \n5). Please link your fflogs. \n6). Do you have any goals as a raider? If so, describe what you hope to accomplish. \n**Please copy/paste the questions and answers into the created text channel named after you in the Enrage Discord.**");
-						}
-						if (message.content === '-Crafter') {
-							message.author.send("3). Please link your Lodestone. \n4). Please link your gear/stats for your gatherer/crafter. \n5). List all mastery/folklore books you have obtained. \n**Please copy/paste the questions and answers into the created text channel named after you in the Enrage Discord.**");
-						}
-						if (message.content === '-Social') {
-							message.author.send("3). Name the Enrage Raider who referred you to this free company. One is required. \n**Please copy/paste the questions and answers into the created text channel named after you in the Enrage Discord.** ");
-						}
-						message.channel.send("Very good. I have sent the application instructions directly to you. Please check your DMs.");
+				.then(function (channel) {
+					channel.overwritePermissions(message.author, {
+						READ_MESSAGES: true,
+						SEND_MESSAGES: true
 					});
+					channel.overwritePermissions(everyone, {
+						READ_MESSAGES: false,
+						SEND_MESSAGES: false
+					});
+					channel.overwritePermissions(officer, {
+						READ_MESSAGES: true,
+						SEND_MESSAGES: true
+					});
+					message.author.send("Here are the questions you are expected to answer. Please answer them thoroughly and to the best of your knowledge, as this is Enrage's first impression of you. \nOnce you have completed all the questions, **please copy/paste the questions and answers into the created text channel named after you in the Enrage Discord.**");
+					message.author.send("You must complete this process within 24 hours. Failure to do so will result in the immediate rejection of your application. \nAlso note that your application will be scrutinized by both the officers and the members.");
+					message.author.send("With that, here are the questions: \n1). What is your character's name? \n2). What made you choose Enrage? What are your expectations of us?");
+					if (message.content === '-Raider') {
+						message.author.send("3). What class(es) do you raid on? \n4). Please link your Lodestone with your Achievements set to be viewed publically. \n5). Please link your fflogs. \n6). Do you have any goals as a raider? If so, describe what you hope to accomplish. \n**Please copy/paste the questions and answers into the created text channel named after you in the Enrage Discord.**");
+					}
+					if (message.content === '-Crafter') {
+						message.author.send("3). Please link your Lodestone. \n4). Please link your gear/stats for your gatherer/crafter. \n5). List all mastery/folklore books you have obtained. \n**Please copy/paste the questions and answers into the created text channel named after you in the Enrage Discord.**");
+					}
+					if (message.content === '-Social') {
+						message.author.send("3). Name the Enrage Raider who referred you to this free company. One is required. \n**Please copy/paste the questions and answers into the created text channel named after you in the Enrage Discord.** ");
+					}
+					message.channel.send("Very good. I have sent the application instructions directly to you. Please check your DMs.");
+				});
 				message.guild.createChannel(message.author.username.replace(/\s+/g, '-').replace(/[^\x00-\x7F]/g, '').replace(/\W/g, '').toLowerCase() + '-discussion', 'text')
-					.then(function (channel) {
-						channel.overwritePermissions(message.author, {
-							READ_MESSAGES: false,
-							SEND_MESSAGES: false
-						});
-						channel.overwritePermissions(everyone, {
-							READ_MESSAGES: false,
-							SEND_MESSAGES: false
-						});
-						channel.overwritePermissions(enragemember, {
-							READ_MESSAGES: false,
-							SEND_MESSAGES: false
-						});
-						channel.overwritePermissions(officer, {
-							READ_MESSAGES: true,
-							SEND_MESSAGES: true
-						});
+				.then(function (channel) {
+					channel.overwritePermissions(message.author, {
+						READ_MESSAGES: false,
+						SEND_MESSAGES: false
 					});
+					channel.overwritePermissions(everyone, {
+						READ_MESSAGES: false,
+						SEND_MESSAGES: false
+					});
+					channel.overwritePermissions(enragemember, {
+						READ_MESSAGES: false,
+						SEND_MESSAGES: false
+					});
+					channel.overwritePermissions(officer, {
+						READ_MESSAGES: true,
+						SEND_MESSAGES: true
+					});
+				});
 			} else {
 				message.channel.send("It seems like you already have an application in place. Please wait for your current application to finish before applying for another role.");
 			}
@@ -142,18 +142,18 @@ client.on('message', message => {
 			}
 			if (message.channel.type === 'text') {
 				message.channel.fetchMessages()
-					.then(messages => {
-						messagesToDelete = messages.filter(function (message) {
-							if (!message.pinned) {
-								return true;
-							}
-							return false;
-						});
-						message.channel.bulkDelete(messagesToDelete);
-						message.channel.send("Messages for #recruitment have been cleared for the week.");
-					}).catch(err => {
-						console.log(err);
+				.then(messages => {
+					messagesToDelete = messages.filter(function (message) {
+						if (!message.pinned) {
+							return true;
+						}
+						return false;
 					});
+					message.channel.bulkDelete(messagesToDelete);
+					message.channel.send("Messages for #recruitment have been cleared for the week.");
+				}).catch(err => {
+					console.log(err);
+				});
 			}
 		}
 		if (message.content.toLowerCase().indexOf('-alliance') > -1) {
@@ -196,18 +196,18 @@ client.on('message', message => {
 			}
 			if (message.channel.type === 'text') {
 				message.channel.fetchMessages()
-					.then(messages => {
-						messagesToDelete = messages.filter(function (message) {
-							if (!message.pinned) {
-								return true;
-							}
-							return false;
-						});
-						message.channel.bulkDelete(messagesToDelete);
-						message.channel.send("Messages for #crafts have been cleared for the week.");
-					}).catch(err => {
-						console.log(err);
+				.then(messages => {
+					messagesToDelete = messages.filter(function (message) {
+						if (!message.pinned) {
+							return true;
+						}
+						return false;
 					});
+					message.channel.bulkDelete(messagesToDelete);
+					message.channel.send("Messages for #crafts have been cleared for the week.");
+				}).catch(err => {
+					console.log(err);
+				});
 			}
 		}
 		if (message.content.toLowerCase().indexOf('-supplies') > -1) {
@@ -247,53 +247,53 @@ client.on('message', message => {
 	}
 	if (message.channel.name === 'entrance-hall') {
 		if (message.content.toLowerCase() === '-clear') {
-					if (!message.channel.permissionsFor(message.author).has("MANAGE_MESSAGES")) {
-						message.channel.send("Apologies, you do not have the permission to execute the command \""+message.content+"\"");
-						return;
-					}
-					if (message.channel.type === 'text') {
-						message.channel.fetchMessages()
-						 .then(messages => {
-							 messagesToDelete = messages.filter(function (message) {
-								if (!message.pinned) {
-									return true;
-									 }
-								return false;
-											});
-										message.channel.bulkDelete(messagesToDelete);
-										message.channel.send("Messages for #entrance-hall have been cleared.");
-									}).catch(err => {
-										console.log(err);
-									});
-					}
+			if (!message.channel.permissionsFor(message.author).has("MANAGE_MESSAGES")) {
+				message.channel.send("Apologies, you do not have the permission to execute the command \""+message.content+"\"");
+				return;
+			}
+			if (message.channel.type === 'text') {
+				message.channel.fetchMessages()
+				.then(messages => {
+					messagesToDelete = messages.filter(function (message) {
+						if (!message.pinned) {
+							return true;
+						}
+						return false;
+					});
+					message.channel.bulkDelete(messagesToDelete);
+					message.channel.send("Messages for #entrance-hall have been cleared.");
+				}).catch(err => {
+					console.log(err);
+				});
+			}
 		}
-}
+	}
 
 	if (message.content.toLowerCase() === '-bacon') {
-	  requested = true;
-	  message.channel.send("Crispy bacon is the only right way.")
+		requested = true;
+		message.channel.send("Crispy bacon is the only right way.")
 	}
 	// lol
-	  if (message.content.toLowerCase() === '-requesterp') {
-	 requested = true;
-	 message.channel.send("Here is the list of sluts at your disposal: ");
-	 message.channel.send("Rai Nagisei: 1 gil");
-		 message.channel.send("Tiamata Pendragon (Little Tia): Free")
-	 message.channel.send("Please indicate which cat slut you would like services from by typing -(name). No space in-between.");
-	 }
-	 if (message.content.toLowerCase() === '-rainagisei' && requested) {
+	if (message.content.toLowerCase() === '-requesterp') {
+		requested = true;
+		message.channel.send("Here is the list of sluts at your disposal: ");
+		message.channel.send("Rai Nagisei: 1 gil");
+		message.channel.send("Tiamata Pendragon (Little Tia): Free")
+		message.channel.send("Please indicate which cat slut you would like services from by typing -(name). No space in-between.");
+	}
+	if (message.content.toLowerCase() === '-rainagisei' && requested) {
 		requested = false;
 		message.channel.send("Very well. Please send a DM to her at your earliest convenience.");
-	 }
-	 if (message.content.toLowerCase() === '-tiamatapendragon' && requested) {
-		 requested = false;
-		 message.channel.send("Very well. Please send a DM to <@107222810081894400> at your earliest convenience.");
-	 }
-	 if (message.content.indexOf('iwi') > -1) {
-		 message.channel.send("uwu")
-	 }
-	 if (message.content.indexOf('IwI') > -1) {
-		 message.channel.send("UwU")
+	}
+	if (message.content.toLowerCase() === '-tiamatapendragon' && requested) {
+		requested = false;
+		message.channel.send("Very well. Please send a DM to <@107222810081894400> at your earliest convenience.");
+	}
+	if (message.content.indexOf('iwi') > -1) {
+		message.channel.send("uwu")
+	}
+	if (message.content.indexOf('IwI') > -1) {
+		message.channel.send("UwU")
 	}
 	if (message.content.indexOf('Iwi') > -1) {
 		message.channel.send("Uwu")
